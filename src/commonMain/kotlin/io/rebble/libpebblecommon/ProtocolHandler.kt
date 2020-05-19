3106e29644a548b2bf5323fd1dc98306ee5a4fd1
crc-32 <me@crc32.dev>
2020-05-19 03:56:51 +0100

    Make handler decode failure non-fatal (skips handling packet) + fix Ping

--- src/commonMain/kotlin/io/rebble/libpebblecommon/ProtocolHandler.kt
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/ProtocolHandler.kt
@@ -1,5 +1,6 @@
 package io.rebble.libpebblecommon
 
+import io.rebble.libpebblecommon.exceptions.PacketDecodeException
 import io.rebble.libpebblecommon.protocol.PacketRegistry
 import io.rebble.libpebblecommon.protocol.PebblePacket
 import io.rebble.libpebblecommon.protocol.ProtocolEndpoint
@@ -15,27 +16,31 @@ class ProtocolHandler(private val send: (ByteArray) -> Unit) {
         send(bytes)
     }
     fun handle(bytes: ByteArray) {
-        val packet = PebblePacket.deserialize(bytes.toUByteArray())
+        try {
+            val packet = PebblePacket.deserialize(bytes.toUByteArray())
 
-        when (packet.endpoint) {
-            ProtocolEndpoint.PING -> {
-                when (packet.type) {
-                    PingPong.Message.Ping.value -> PingPong.Pong((packet as PingPong.Ping).cookie.get())
-                    PingPong.Message.Pong.value -> println("Pong! ${(packet as PingPong.Pong).cookie.get()}")
+            when (packet.endpoint) {
+                ProtocolEndpoint.PING -> {
+                    when (packet.type) {
+                        PingPong.Message.Ping.value -> send(PingPong.Pong((packet as PingPong.Ping).cookie.get()).serialize().toByteArray())
+                        PingPong.Message.Pong.value -> println("Pong! ${(packet as PingPong.Pong).cookie.get()}")
+                    }
                 }
-            }
 
-            ProtocolEndpoint.PHONE_VERSION -> {
-                val res = PhoneAppVersion.AppVersionResponse()
-                res.protocolVersion.set(1u)
-                res.majorVersion.   set(2u)
-                res.minorVersion.   set(2u)
-                res.bugfixVersion.  set(0u)
-                //TODO: Platform flags + CAPS
-                _send(res.serialize().toByteArray(), packet)
-            }
+                ProtocolEndpoint.PHONE_VERSION -> {
+                    val res = PhoneAppVersion.AppVersionResponse()
+                    res.protocolVersion.set(1u)
+                    res.majorVersion.   set(2u)
+                    res.minorVersion.   set(2u)
+                    res.bugfixVersion.  set(0u)
+                    //TODO: Platform flags + CAPS
+                    _send(res.serialize().toByteArray(), res)
+                }
 
-            else -> println("TODO: ${packet.endpoint}")
+                else -> println("TODO: ${packet.endpoint}")
+            }
+        }catch (e: PacketDecodeException){
+            println("Warning: failed to decode a packet: '${e.message}'")
         }
     }
 }
