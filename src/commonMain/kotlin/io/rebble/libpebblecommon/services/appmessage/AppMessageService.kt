e536c44374b25664fc8d40b3f1b1812a85302377
Matej Drobnic <matejdro@gmail.com>
2020-10-11 07:55:31 +0200

    fix AppMessageService comments

--- src/commonMain/kotlin/io/rebble/libpebblecommon/services/appmessage/AppMessageService.kt
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/services/appmessage/AppMessageService.kt
@@ -2,12 +2,8 @@ package io.rebble.libpebblecommon.services.appmessage
 
 import io.rebble.libpebblecommon.ProtocolHandler
 import io.rebble.libpebblecommon.packets.AppMessage
-import io.rebble.libpebblecommon.packets.blobdb.BlobCommand
-import io.rebble.libpebblecommon.packets.blobdb.BlobResponse
 import io.rebble.libpebblecommon.protocolhelpers.PebblePacket
 import io.rebble.libpebblecommon.protocolhelpers.ProtocolEndpoint
-import io.rebble.libpebblecommon.structmapper.SUByte
-import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 
 class AppMessageService(private val protocolHandler: ProtocolHandler) {
@@ -18,10 +14,7 @@ class AppMessageService(private val protocolHandler: ProtocolHandler) {
     }
 
     /**
-     * Send a BlobCommand, with an optional callback to be triggered when a matching BlobResponse is received
-     * @see BlobCommand
-     * @see BlobResponse
-     * @param packet the packet to send
+     * Send an AppMessage
      */
     suspend fun send(packet: AppMessage) {
         protocolHandler.withWatchContext {
@@ -29,12 +22,6 @@ class AppMessageService(private val protocolHandler: ProtocolHandler) {
         }
     }
 
-    /**
-     * Intended to be called via a protocol handler, handles BlobResponse packets
-     * @return true if the packet was handled, false if it wasn't (e.g. not sent via send())
-     * @see send
-     * @see BlobResponse
-     */
     @OptIn(ExperimentalUnsignedTypes::class)
     fun receive(packet: PebblePacket) {
         if (packet !is AppMessage) {
