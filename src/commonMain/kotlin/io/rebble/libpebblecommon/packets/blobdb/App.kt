a10656d5ade62d02c0fba2710d0b36b6394e16bf
Matej Drobnic <matejdro@gmail.com>
2021-03-11 20:47:29 +0100

    add app blobdb metadata

---
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/packets/blobdb/App.kt
@@ -0,0 +1,59 @@
+package io.rebble.libpebblecommon.packets.blobdb
+
+import io.rebble.libpebblecommon.structmapper.*
+
+/**
+ * Data of the APP BlobDB Entry
+ */
+class AppMetadata() : StructMappable() {
+    /**
+     * UUID of the app
+     */
+    val uuid: SUUID = SUUID(m)
+
+    /**
+     * App install flags.
+     */
+    val flags: SUInt = SUInt(m)
+
+    /**
+     * Resource ID of the primary icon.
+     */
+    val icon: SUInt = SUInt(m)
+
+    /**
+     * Major app version.
+     */
+    val appVersionMajor: SUByte = SUByte(m)
+
+    /**
+     * Minor app version.
+     */
+    val appVersionMinor: SUByte = SUByte(m)
+
+    /**
+     * Major sdk version.
+     */
+    val sdkVersionMajor: SUByte = SUByte(m)
+
+    /**
+     * Minor sdk version.
+     */
+    val sdkVersionMinor: SUByte = SUByte(m)
+
+    /**
+     * ??? (Always sent as 0 in the Pebble app)
+     */
+    val appFaceBgColor: SUByte = SUByte(m, 0u)
+
+    /**
+     * ??? (Always sent as 0 in the Pebble app)
+     */
+    val appFaceTemplateId: SUByte = SUByte(m, 0u)
+
+    /**
+     * Name of the app
+     */
+    val appName: SFixedString = SFixedString(m, 96)
+}
+
