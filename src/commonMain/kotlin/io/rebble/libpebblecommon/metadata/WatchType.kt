4dcb5812eeb295ee9f70d29b5eab1911530c5bea
Matej Drobnic <matejdro@gmail.com>
2021-03-11 20:47:51 +0100

    add list of watch platforms

---
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/metadata/WatchType.kt
@@ -0,0 +1,9 @@
+package io.rebble.libpebblecommon.metadata
+
+enum class WatchType(val codename: String) {
+    APLITE("aplite"),
+    BASALT("basalt"),
+    CHALK("chalk"),
+    DIORITE("diorite"),
+    EMERY("emery")
+}
