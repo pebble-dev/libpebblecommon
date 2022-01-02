15e93991411e3d90316815e94e3a0b0ff454ad67
crc-32 <me@crc32.dev>
2022-01-02 22:35:06 +0000

    Add interop utils (hacks, basically) to be used by iOS

--- src/commonMain/kotlin/io/rebble/libpebblecommon/util/UtilFunctions.kt
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/util/UtilFunctions.kt
@@ -1,3 +1,16 @@
 package io.rebble.libpebblecommon.util
 
 expect fun runBlocking(block: suspend () -> Unit)
+
+object KUtil {
+    fun byteArrayAsUByteArray(arr: ByteArray): UByteArray = arr.asUByteArray()
+    fun uByteArrayAsByteArray(arr: UByteArray): ByteArray = arr.asByteArray()
+}
+
+infix fun Byte.ushr(bitCount: Int): Byte = ((this.toInt()) ushr bitCount).toByte()
+infix fun Byte.shl(bitCount: Int): Byte = ((this.toInt()) shl bitCount).toByte()
+infix fun UByte.shr(bitCount: Int): UByte = ((this.toUInt()) shr bitCount).toUByte()
+infix fun UByte.shl(bitCount: Int): UByte = ((this.toUInt()) shl bitCount).toUByte()
+
+infix fun Short.shl(bitCount: Int): Short = ((this.toInt()) shl bitCount).toShort()
+infix fun UShort.shl(bitCount: Int): UShort = ((this.toUInt()) shl bitCount).toUShort()
--- src/iosMain/kotlin/util/UtilFunctions.kt
+++ src/iosMain/kotlin/util/UtilFunctions.kt
@@ -1,6 +1,13 @@
 package io.rebble.libpebblecommon.util
 
 import kotlinx.cinterop.*
+import platform.Foundation.NSArray
+import platform.Foundation.NSData
+import platform.Foundation.create
+import platform.darwin.UInt8
+import platform.darwin.UInt8Var
+import platform.posix.memcpy
+import kotlin.native.internal.NativePtr
 
 actual fun runBlocking(block: suspend () -> Unit) = kotlinx.coroutines.runBlocking{block()}
 
@@ -18,3 +25,15 @@ internal fun reverseOrd(varr: UShort): UShort = (((varr.toInt() and 0xff) shl 8)
 internal fun reverseOrd(varr: UInt): UInt = ((reverseOrd((varr and 0xffffu).toUShort()).toInt() shl 16) or (reverseOrd((varr shr 16).toUShort()).toInt() and 0xffff)).toUInt()
 
 internal fun reverseOrd(varr: ULong): ULong = ((reverseOrd((varr and 0xffffffffu).toUInt()).toLong() shl 32) or (reverseOrd((varr shr 32).toUInt()).toLong() and 0xffffffff)).toULong()
+
+fun ByteArray.toNative(): NSData = memScoped {
+    NSData.create(bytes = allocArrayOf(this@toNative), length = this@toNative.size.toULong())
+}
+
+fun KUtil.byteArrayFromNative(arr: NSData): ByteArray = ByteArray(arr.length.toInt()).apply {
+    if (this.isNotEmpty()) {
+        usePinned {
+            memcpy(it.addressOf(0), arr.bytes, arr.length)
+        }
+    }
+}
