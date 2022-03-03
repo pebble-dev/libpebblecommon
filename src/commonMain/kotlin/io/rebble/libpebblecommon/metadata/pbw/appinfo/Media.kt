fc224dd7b5c995ac1c6bbdcbedaad25be01598f4
crc-32 <me@crc32.dev>
2022-03-03 02:55:29 +0000

    Fix deserialization of appinfo

--- src/commonMain/kotlin/io/rebble/libpebblecommon/metadata/pbw/appinfo/Media.kt
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/metadata/pbw/appinfo/Media.kt
@@ -3,12 +3,13 @@ package io.rebble.libpebblecommon.metadata.pbw.appinfo
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
+import io.rebble.libpebblecommon.metadata.StringOrBoolean
 
 @Serializable
 data class Media(
     @SerialName("file")
     val resourceFile: String,
-    val menuIcon: Boolean = false,
+    val menuIcon: StringOrBoolean = StringOrBoolean(false),
     val name: String,
     val type: String
 )
