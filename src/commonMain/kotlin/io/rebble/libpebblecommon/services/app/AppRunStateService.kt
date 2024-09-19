fc1285e93cc5d82c79bb96e33dc60858510959ca
crc-32 <me@crc32.dev>
2024-09-19 16:37:38 +0100

    apprunstate util functions

--- src/commonMain/kotlin/io/rebble/libpebblecommon/services/app/AppRunStateService.kt
+++ src/commonMain/kotlin/io/rebble/libpebblecommon/services/app/AppRunStateService.kt
@@ -1,11 +1,14 @@
 package io.rebble.libpebblecommon.services.app
 
+import com.benasher44.uuid.Uuid
 import io.rebble.libpebblecommon.ProtocolHandler
 import io.rebble.libpebblecommon.packets.AppRunStateMessage
 import io.rebble.libpebblecommon.protocolhelpers.PebblePacket
 import io.rebble.libpebblecommon.protocolhelpers.ProtocolEndpoint
 import io.rebble.libpebblecommon.services.ProtocolService
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.receiveAsFlow
 
 class AppRunStateService(private val protocolHandler: ProtocolHandler) : ProtocolService {
     val receivedMessages = Channel<AppRunStateMessage>(Channel.BUFFERED)
@@ -24,6 +27,14 @@ class AppRunStateService(private val protocolHandler: ProtocolHandler) : Protoco
         }
 
         receivedMessages.trySend(packet)
+    }
+
+    suspend fun startApp(uuid: Uuid) {
+        send(AppRunStateMessage.AppRunStateStart(uuid))
+    }
+
+    suspend fun stopApp(uuid: Uuid) {
+        send(AppRunStateMessage.AppRunStateStop(uuid))
     }
 
 }
